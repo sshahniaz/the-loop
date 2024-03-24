d08e2388f8129eaab184a658370595e3f5561736
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-03-24 11:01:58 +0000

    Footer

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,90 @@
+// import React from "react";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import XIcon from "@mui/icons-material/X";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import Link from "next/link";
+import "./Footer.scss";
+// import EmailSignup from "./emailsignup/EmailSignup";
+const Footer = () => {
+  return (
+    <>
+      <div className="footerContainer">
+        <div className="categories">
+          <h4>Categories</h4>
+          <ul>
+            <li>
+              <Link href="#">Furniture</Link>
+            </li>
+            <li>
+              <Link href="#">Lighting</Link>
+            </li>
+            <li>
+              <Link href="#">Home Decor</Link>
+            </li>
+            <li>
+              <Link href="#">Art</Link>
+            </li>
+            <li>
+              <Link href="#">Kitchenware</Link>
+            </li>
+            <li>
+              <Link href="#">Sale</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="help">
+          <h4>Help</h4>
+          <ul>
+            <li>
+              <Link href="#">How To Sell</Link>
+            </li>
+            <li>
+              <Link href="#">Contact Us</Link>
+            </li>
+            <li>
+              <Link href="#">Terms & Conditions</Link>
+            </li>
+            <li>
+              <Link href="#">FAQ's</Link>
+            </li>
+          </ul>
+          <div className="icons">
+            <ul>
+              <li className="listIcon">
+                <a href="#">
+                  <InstagramIcon />
+                </a>
+              </li>
+              <li className="listIcon">
+                <a href="#">
+                  <XIcon />
+                </a>
+              </li>
+              <li className="listIcon">
+                <a href="#">
+                  <FacebookIcon />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <p id="est">The LOOP Established 2024.</p>
+          <div className="footerLogo">
+            <Link href="#">
+              <img src="./assets/logowhite.svg" alt="Logo" />
+            </Link>
+          </div>
+        </div>
+        <div className="signup">
+          <h4>Signup To Our Newsletter</h4>
+          <p className="signupPara">
+            Get a weekly dose of inspiration & tips on living sustainably
+          </p>
+          {/* <EmailSignup /> */}
+          {/* form component here */}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navigation from "./components/navigation/Navigation";
+import Footer from "./components/footer/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <Navigation />
         {children}
+        <Footer />
       </body>
     </html>
   );
