380c33ad2a8bcf569cc52a5dbf7c097443860f83
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-03-27 09:55:40 +0000

    dynamic routing functionality fixed

--- app/api/profile/[customerId]/route.tsx
+++ app/api/profile/[customerId]/route.tsx
@@ -2,16 +2,43 @@ import prisma from "@/prisma/client";
 
 import { NextResponse } from "next/server";
 
-export async function GET() {
-  try {
-    const products = await prisma.product.findMany({
-      where: {
-        ownerId: "65fc1d82bffb6b8984064dd4",
-      },
-    });
+// export default function handler(req: Request, res: Response) {
+//   const url = new URL(req.url, );
 
-    return NextResponse.json(products);
-  } catch (error) {
-    return NextResponse.json({ error: "Error fetching data" });
+export async function GET(request: Request) {
+  if (request.method === "GET") {
+    try {
+      // get the customerId from the url
+      const url = new URL(request.url, `http://${request.headers.get("host")}`);
+      console.log(url);
+      const { pathname } = url;
+      const customerId = pathname.split("/").slice(-1)[0];
+      // const customerId = "65faf8493a25aae6e6aedda3";
+      // const parsedUrl = new URL(url, 'http://localhost:3000');
+      // const userId = parsedUrl.searchParams.get()
+      console.log(customerId);
+      const products = await prisma.product.findMany({
+        where: {
+          ownerId: customerId,
+        },
+      });
+      // console.log(products);
+      // may need to use Response instead of NextResponse
+      return NextResponse.json(products);
+    } catch (error) {
+      return NextResponse.json({ error: "Error fetching data" });
+    }
   }
 }
+
+// try {
+//   const products = await prisma.product.findMany({
+//     where: {
+//       ownerId: "65fc1d82bffb6b8984064dd4",
+//     },
+//   });
+
+//   return NextResponse.json(products);
+// } catch (error) {
+//   return NextResponse.json({ error: "Error fetching data" });
+// }
--- app/profile/[customerId]/page.tsx
+++ app/profile/[customerId]/page.tsx
@@ -1,27 +1,22 @@
 "use client";
-import { useState, useEffect, use } from "react";
-
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import { usePathname } from "next/navigation";
-
-interface ProductApiResponse {
-  products: Product[];
-}
-
-interface Product {
-  name: string;
-  details: string;
-  image: string;
-}
-
 export default function ProfilePage() {
+  // get the pathname
   const pathname = usePathname();
+  const searchParams = useSearchParams();
+
   const [products, setProducts] = useState<any[]>([]);
   const [loading, isLoading] = useState(false);
   const [error, setError] = useState(null);
   useEffect(() => {
     const fetchUser = async () => {
       // url to api endpoint
-      const URL = "http://localhost:3000/api/profile/";
+      const URL = `http://localhost:3000/api/${pathname}?${searchParams}`;
+
+      // const URL = `http://localhost:3000/api/profile/65faf8493a25aae6e6aedda3`;
+      console.log(pathname);
       // fetch data from end point and store in the data object
       const data = await fetch(URL).then((res) => res.json());
 
@@ -33,7 +28,7 @@ export default function ProfilePage() {
 
   return (
     <>
-      <h1>{pathname}</h1>
+      <h1>{searchParams}</h1>
       <ul>
         {products.map((product, index) => (
           <li key={index}>{product.name}</li>
