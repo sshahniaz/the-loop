caac031afb371aac6ffb7709ca7a0e3d78008932
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-04-03 16:58:33 +0100

    split client and server actions

---
+++ app/actions/actions.tsx
@@ -0,0 +1,57 @@
+"use server";
+import prisma from "@/prisma/client";
+import { revalidatePath } from "next/cache";
+
+interface ProductData {
+  name: string;
+  details: string;
+  condition: string;
+  type: string;
+  category: string;
+  subCategory: string;
+  price: number;
+  colour: string;
+  material: string;
+}
+
+export async function listItem(formData: FormData) {
+  const name = formData.get("name");
+  const details = formData.get("details");
+  const condition = formData.get("condition");
+  const type = formData.get("type");
+  const category = formData.get("category");
+  const subCategory = formData.get("subCategory");
+  const price = formData.get("price");
+  const colour = formData.get("colour");
+  const material = formData.get("material");
+
+  const product = await prisma.product.create({
+    data: {
+      name: name as string,
+      details: details as string,
+      price: Number(formData.get("price")) as number,
+      discount: 0,
+      imageLink: [
+        "/assets/stock-photos/lighting/ceilinglight1.png",
+        "/assets/stock-photos/lighting/ceilinglight1.png",
+      ],
+
+      colour: colour as string,
+      material: material as string,
+      type: type as string,
+      catagory: category as string,
+      subCatagory: subCategory as string,
+      sale: 0,
+      condition: condition as string,
+      owner: {
+        connect: {
+          id: "65faf8493a25aae6e6aedda2",
+        },
+      },
+    },
+  });
+
+  revalidatePath("/sell/form");
+
+  console.log("data:", formData);
+}
