a7bd2872eed83949a8cd849ae3d71de92db3c6b3
sshahniaz <100251uk@saeinstitute.edu>
2024-04-04 11:07:19 +0100

    Fixed action.tsx

--- app/components/navigation/QuickLinks.tsx
+++ app/components/navigation/QuickLinks.tsx
@@ -36,38 +36,6 @@ const QuickLinks = () => {
           <QuickLinksItem linkItems={links} />
         </ul>
       </div>
-      {/* <QuickLinksItem LinkProps={links[0]} /> */}
-
-      <ul>
-      {links.map((pageR, index) => (
-        <QuickLinksItem key={index} href={pageR}/>
-      ))}
-        {/* <li>
-          <Link href={`/categories/${pageR}`}> lighting </Link>
-        </li> */}
-      </ul>
-      {/* <ul>
-        <li>
-          <img src="" alt="" />
-          <Link href="#">Lighting</Link>
-        </li>
-        <li>
-          <img src="" alt="" />
-          <Link href="#">Chairs</Link>
-        </li>
-        <li>
-          <img src="" alt="" />
-          <Link href="#">Posters</Link>
-        </li>
-        <li>
-          <img src="" alt="" />
-          <Link href="#">Sofas</Link>
-        </li>
-        <li>
-          <img src="" alt="" />
-          <Link href="#">Bedroom Furniture</Link>
-        </li>
-      </ul> */}
     </>
   );
 };
