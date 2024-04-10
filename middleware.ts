5b185cfb633b114529a3702f73ae05ffc43b0594
Gabriel Dawson-Amoah <gdawsonamoah@yahoo.com>
2024-04-10 23:00:47 +0100

    Changed the hero images

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
-  publicRoutes: ["/", "/faq", "/signup", "/login", "/sell"],
+  publicRoutes: ["/", "/faq", "/sell"],
 });
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
