dbed6e7b57e3b58913dc6725ec96704697d3488e
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-04-12 18:45:56 +0100

    styling of search page

---
+++ app/components/ImageHover.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface Props {
+  image: string;
+}
+
+const ImageHover = ({ image }: Props) => {
+  return (
+    <div>
+      <img></img>
+    </div>
+  );
+};
+
+export default ImageHover;
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,11 +3,20 @@ import { useSearchParams } from "next/navigation";
 import useSWR from "swr";
 import React from "react";
 import "./SearchResults.scss";
+import AddToWishList from "../components/wishlist/AddToWishList";
+import ImageHover from "../components/ImageHover";
 
 interface Product {
+  id: string;
   name: string;
   price: number;
   imageLink: string;
+}
+
+interface AddToWishlistProps {
+  productId: string;
+  userId?: string; // Optional user ID
+  onUpdateWishlist?: () => void; // Optional callback to update wishlist state in parent component
 }
 
 const fetchProducts = async (url: string) => {
@@ -25,6 +34,7 @@ const page = () => {
     `/api/search?q=${encodedSearchQuery}`,
     fetchProducts
   );
+  console.log(data);
 
   if (data && data.products && data.products.length > 0) {
     // console.log(data.products);
@@ -32,16 +42,33 @@ const page = () => {
 
   return (
     <>
-      <div className="searchResultContainer">
-        {data?.products?.map((product, index) => (
-          <div className="searchResultCard" key={index}>
-            <img src={product.imageLink[0]} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>{product.price}</p>
-          </div>
-        ))}
+      <div className="pageContainer">
+        {/* need to add filter here */}
+        <div className="searchResultContainer">
+          {data?.products?.map((product, index) => (
+            <div className="searchResultCard" key={index}>
+              {/* <ImageHover image={product.imageLink} /> */}
+              <img
+                src={product.imageLink[0]}
+                alt={product.name}
+                className="searchImg"
+              />
+              <div className="iconFloat">
+                <h3 className="productHeading">{product.name}</h3>
+                <AddToWishList
+                  productId={product.id}
+                  userId="65fc1d82bffb6b8984064dd3"
+                />
+                {/* <button>
+                  <FavoriteBorderOutlinedIcon />
+                </button> */}
+              </div>
+              <p>£{product.price}</p>
+            </div>
+          ))}
+        </div>
+        .
       </div>
-      .
     </>
   );
 };
