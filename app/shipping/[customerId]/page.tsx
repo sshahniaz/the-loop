6518c0e9db350a0cc058508d613d8bd6a17e4cc5
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-04-17 21:04:28 +0100

    Update page.tsx

--- app/shipping/[customerId]/page.tsx
+++ app/shipping/[customerId]/page.tsx
@@ -1,23 +1,17 @@
 "use client";
-import React, { useState, Suspense } from 'react'
-import ShippingMainContainer from '@/app/components/ShippingInfo/ShippingMainContainer';
+import React, { useState, Suspense } from "react";
+import ShippingMainContainer from "@/app/components/ShippingInfo/ShippingMainContainer";
 
 // import useSWR from 'swr'
 
-
-const Page = ({ params}: {params: {customerId: string}}) => {
-  
- 
-
+const Page = ({ params }: { params: { customerId: string } }) => {
   return (
     <div>
       <ShippingMainContainer userId={params.customerId} />
     </div>
-  )
+  );
 
+  // im so relieved
+};
 
-}
-
-
-
-export default Page
+export default Page;
