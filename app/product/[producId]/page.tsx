27da0ba20d84dd2f55ec84b2b1a71d8e7c671e6e
sshahniaz <100251uk@saeinstitute.edu>
2024-04-17 18:25:48 +0100

    Update page.tsx

--- app/product/[producId]/page.tsx
+++ app/product/[producId]/page.tsx
@@ -1,31 +1,30 @@
+"use client";
 import getProductById from "@/app/actions/SingleProductActions";
 import SimilarProduct from "../../components/product/SimilarProduct";
 import ProductDetails from "@/app/components/product/ProductDetails";
 import ProductCard from "@/app/components/product/ProductCard";
-
+import React,{ useEffect, useState } from "react";
 interface IParams {
   productId: string;
 }
 
 const Page = ({ params }: { params: IParams }) => {
-  const fetchProducts = async () => {
-    try {
+  const [ product, setProduct ] = useState<any>(null);
+  useEffect(() => {
+    const fetchProduct = async () => {
       const product = await getProductById(params);
-      return product;
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  console.log("params", params);
-
-  const returnedProducts = fetchProducts();
-  if (!returnedProducts) return <p>Product doesn't exist</p>;
+      setProduct(product);
+    };
+    fetchProduct();
+  }, [params]);
+  
+  if (!product) return <p>Product doesn't exist</p>;
 
   return (
     <>
       <div>
-        <ProductDetails product={returnedProducts} />
-        <ProductCard product={returnedProducts} />
+        <ProductDetails product={product} />
+        <ProductCard product={product} />
       </div>
     </>
   );
