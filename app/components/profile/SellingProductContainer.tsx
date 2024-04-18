3b6691cf50720627002a2ec6b569738305c0ab2d
Leigh Richards <73237857+Winifreddo@users.noreply.github.com>
2024-04-18 14:51:32 +0100

    Update sellingg container

--- app/components/profile/SellingProductContainer.tsx
+++ app/components/profile/SellingProductContainer.tsx
@@ -1,5 +1,5 @@
-import React,{useState, useEffect, ChangeEvent} from 'react'
-import { updateProduct } from '@/app/actions/actions'
+import React, { useState, useEffect, ChangeEvent } from "react";
+import { updateProduct } from "@/app/actions/actions";
 import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
 import { DeleteItem } from "@/app/actions/SellingActions";
@@ -11,19 +11,20 @@ interface ProductProps {
     name: string;
     price: number;
     imageLink: string[];
-  }
+  };
   onDelete: (productId: string) => void;
 }
 
-const SellingProductContainer = ({ product: {id, name, price, imageLink}, onDelete }: ProductProps) => {
-  
+const SellingProductContainer = ({
+  product: { id, name, price, imageLink },
+  onDelete,
+}: ProductProps) => {
   const [isEdit, setIsEdit] = useState(false);
-  const [ productData, setProductData ] = useState({
+  const [productData, setProductData] = useState({
     id,
     name,
     price,
     imageLink,
-  
   });
 
   const handleEdit = () => {
@@ -39,79 +40,82 @@ const SellingProductContainer = ({ product: {id, name, price, imageLink}, onDele
   };
 
   const handleCancel = () => {
-    setProductData({id, name, price, imageLink})// Reset to original data
+    setProductData({ id, name, price, imageLink }); // Reset to original data
     setIsEdit(false);
   };
 
-
   return (
-    <div> {isEdit ? (
-            <form>
-              <div>
-                <label htmlFor="name">Name</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={productData.name}
-                  onChange={handleChange}
-                />
-                <label htmlFor="price">Price</label>
-                <input
-                  type="text"
-                  id="price"
-                  name="price"
-                  value={productData.price}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="dashboardButtons">
-                <button type="button" onClick={handleSave}>
-                  <DoneIcon />
-                </button>
-                <button type="button" onClick={handleCancel}>
-                  <CloseIcon />
-                </button>
-              </div>
-            </form>
-          ) : (
-            <li key={id}>
-              <div className="listingFlex">
-                <div className="sellingImage">
-                  <img src={productData.imageLink[0]} alt={productData.name} />
+    <div>
+      {" "}
+      {isEdit ? (
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={productData.name}
+              onChange={handleChange}
+            />
+            <label htmlFor="price">Price</label>
+            <input
+              type="text"
+              id="price"
+              name="price"
+              value={productData.price}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="dashboardButtons">
+            <button type="button" onClick={handleSave}>
+              <DoneIcon />
+            </button>
+            <button type="button" onClick={handleCancel}>
+              <CloseIcon />
+            </button>
+          </div>
+        </form>
+      ) : (
+        <li key={id}>
+          <div className="listingFlex">
+            <div className="sellingImage">
+              <img src={productData.imageLink[0]} alt={productData.name} />
+            </div>
+            <div className="sellingDetails">
+              <p className="sellingName">{productData.name} -</p>{" "}
+              <p className="sellingName">£{productData.price}</p>
+              <div className="detailsFlex">
+                <div className="listingFlex1">
+                  <p>Revise Listing</p>
+                  <button onClick={handleEdit}>
+                    <CreateOutlinedIcon />
+                  </button>
                 </div>
-                <div className="sellingDetails">
-                  <p className="sellingName">{productData.name} -</p>{" "}
-                  <p className="sellingName">£{productData.price}</p>
-                  <div className="detailsFlex">
-                    <div className="listingFlex1">
-                      <p>Revise Listing</p>
-                      <button onClick={handleEdit}>
-                        <CreateOutlinedIcon />
-                      </button>
-                    </div>
-                    <div className="listingFlex1">
-                      <p>Delete Listing</p>
-                      <button
-                        onClick={() => {
-                          if (
-                            window.confirm(
-                              `Are you sure you want to delete ${productData.name}?`
-                            )
-                          ) {
-                            onDelete(productData.id);
-                          }
-                        }}
-                      >
-                        <DeleteOutlineIcon />
-                      </button>
-                    </div>
-                  </div>
+                <div className="listingFlex1">
+                  <p>Delete Listing</p>
+                  <button
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          `Are you sure you want to delete ${productData.name}?`
+                        )
+                      ) {
+                        onDelete(productData.id);
+                      }
+                    }}
+                  >
+                    <DeleteOutlineIcon />
+                  </button>
                 </div>
               </div>
-            </li>
-          )}</div>
-  )
-}
+              <hr />
+            </div>
+          </div>
+        </li>
+      )}
+    </div>
+  );
+};
 
-export default SellingProductContainer
+export default SellingProductContainer;
