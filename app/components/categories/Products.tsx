eb5f543f309b3a7b48a632830d2979a361a98a59
irinadica <irina_andreea_dica@yahoo.com>
2024-04-18 10:46:09 +0100

    Products page

--- app/components/categories/Products.tsx
+++ app/components/categories/Products.tsx
@@ -1,30 +1,43 @@
-import React from 'react'
-
-
-interface Product{
+import React from "react";
+import ImageHover from "../ImageHover";
+import AddToWishList from "../wishlist/AddToWishList";
+import "../../search/SearchResults.scss";
+interface Product {
   id: string;
   name: string;
   price: number;
   colour: string;
   material: string;
   condition: string;
+  imageLink: string;
 }
 
 const Products = ({ products }: { products: Product[] }) => {
-
+  const handlwWishlistupdate = async () => {
+    console.log("wishlist updated");
+  };
   return (
-     <div>
-      <h2>Products</h2>
-      {products.map((product) => (
-        <div key={product.id}>
-          {/* Display product information here */}
-          <p>{product.name} - £{ product.price }</p>
-          
-
+    <>
+      <div className="pageContainer">
+        <div className="searchResultContainer">
+          <h2>Products</h2>
+          {products.map((product) => (
+            <div className="searchResultCard" key={product.id}>
+              <ImageHover image={product.imageLink} alt={product.name} />
+              <div className="iconFloat">
+                <h3 className="productHeading">{product.name}</h3>
+                <AddToWishList
+                  productId={product.id}
+                  onUpdateWishlist={handlwWishlistupdate}
+                />
+              </div>
+              <p>£{product.price}</p>
+            </div>
+          ))}
         </div>
-      ))}
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default Products
+export default Products;
