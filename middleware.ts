89d73193894a0619b89cbc9a2b227ae77d2ce266
sshahniaz <100251uk@saeinstitute.edu>
2024-06-18 18:16:40 +0100

    updated

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,10 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Allow signed out users to access the specified routes:
-  publicRoutes: ["/", "/faq", "/search","/search/(.*)", "/furniture", "/categories/(.*)", "/categories/furniture", "/categories/lighting", "/categories/Home Decor", "/categories/New", "/categories/sale", "/contactus","/cart","/product/(.*)","/sale"],
+  publicRoutes: ["/", "/faq", "/search","/search/(.*)","/api/search","/api/search/(.*)", "/furniture", "/categories/(.*)", "/categories/furniture", "/categories/lighting", "/categories/Home Decor", "/categories/New", "/categories/sale", "/contactus","/cart","/product/(.*)","/sale"],
 });
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [ "/((?!.+\\.[\\w]+$|_next).*)","/","/(api|trpc)(.*)" ],
+  
+  //  "/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"
 };
